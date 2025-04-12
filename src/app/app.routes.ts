1c262023f050347dadc77a64ec44c6b46e77dfdc
Indium01blr <blr0425ha001@indium.tech>
2025-04-12 14:01:42 +0530

    Changes in Routing config file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { WatchlistComponent } from './components/watchlist/watchlist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "Home" , component : HomeComponent },
+    { path: "Login", component : LoginComponent },
+    { path: "Portfolio", component : PortfolioComponent },
+    { path: "WatchList", component: WatchlistComponent},
+    { path: "Dashboard", component: DashboardComponent }
+];
